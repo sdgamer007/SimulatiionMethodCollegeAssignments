2481ebdb50a50414ece53b1b669b7ed8bb2a1810
sdgamer007 <sohaildua38@gmail.com>
2020-11-05 20:15:53 +0100

    getting to know

---
+++ GettingtoKnow/Array.m
@@ -0,0 +1,33 @@
+x = [ 1 2 3 4 5 ];
+y = zeros(size(x));
+for i = 1:5
+y(i) = x(i)^2;
+end
+
+
+sizx = size(x); %1 row 5 column
+a=[1;2;3;4;5];
+siza = size(a);
+
+mulax= a*x;
+
+zeros(5,2)
+ones(4,3)
+eye(4)
+
+rand()
+rand(2,3)
+
+c = [7 9 5; 6 1 9; 4 3 2]
+
+as(:, :, 2)= [ 1 2 3; 4 5 6; 7 8 9]
+
+bx = rand(4,3,2)
+
+
+x = [7.1, 3.4, 7.2, 28/4, 3.6, 17, 9.4, 8.9];
+length(x)      % length of x vector
+y = rand(3, 4, 5, 2)
+ndims(y)       % no of dimensions in array y
+s = ['Zara', 'Nuha', 'Shamim', 'Riz', 'Shadab'];
+numel(s)       % no of elements in s
